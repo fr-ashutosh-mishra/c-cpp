fa29dc45322cb395aa355cfb59c53db7ced85e2c
fr-ashutosh-mishra <fr.ashutosh.mishra@gmail.com>
2026-03-13 18:26:42 +0530

    Create basicquestionBit.c
    Basic question of bit manipulation

---
+++ basicquestionBit.c
@@ -0,0 +1,70 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+
+int main()
+{
+
+//  int n=9;
+ 
+
+ // count set bits
+ 
+  int count = 0;
+//   while(n){
+//       n= n&(n-1);
+//       count++;
+//   }
+//   printf("%d",count);
+  
+  // check num is powerof2
+  
+//   if(n&n-1){
+//       printf("No is not powerof2");
+//   }
+//   else
+//   {
+//       printf("Yes is a powerof2");
+//   }
+
+
+
+int a=19;
+
+int nth = 4;
+
+// setbit
+// a|= 1 << nth-1;
+
+// clear bit
+// a &= ~(1 << nth-1);
+
+// toogle bit
+// a ^= (1 <<nth-1);
+
+// odd/even
+// if(a&1) printf("odd");
+// else printf("even");
+
+//swap 2 numvers
+
+// int n=10;
+// int m=20;
+
+// n = n^m;
+// m = n^m;
+// n = n^m;
+
+printf("%d %d",n,m);
+  
+ 
+ 
+ 
+ 
+}
