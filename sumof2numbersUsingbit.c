2bede9229dcc284cf4120823d773b82fb81618f0
fr-ashutosh-mishra <fr.ashutosh.mishra@gmail.com>
2026-03-26 15:57:48 +0530

    Update sumof2numbersUsingbit.c

--- sumof2numbersUsingbit.c
+++ sumof2numbersUsingbit.c
@@ -10,26 +10,27 @@ Write your code in this editor and press "Run" button to compile and execute it.
 
 int main()
 {
-    int a=66;
-    int b=45;
-    
-    
-    int c=0,i=0,sum=0;
-    while(a||b||c)
+    int a = 10;
+    int b = 1;
+    int sum=0;
+    int carry = 0;
+    int i=0;
+    while(a||b||carry)
     {
-        int aa=a&1;
-        int bb=b&1;
+        int aa = a&1;
+        int bb = b&1;
         
-        int temp = (aa^bb)^c;
-        sum |= temp << i++;
-        c = (a&b) | (a&c) | (c&b);
+        int temp = aa^bb^carry;
+                
+        temp = temp << i++;
+        sum|=temp;
+        
+        carry = (aa&bb) | (bb&carry) | (carry&aa);
         a = a>>1;
         b = b>>1;
-        
-        
+       
     }
-    
-    printf("%d",sum);
 
+printf("%d",sum);
     return 0;
 }
